b70b8379b0c6b72121330c23aad2fbc1e137a2e9
noideas4thisname <noideas4thisname@hotmail.com>
2020-12-04 13:29:46 -0800

    Initial commit - not yet complete for day 1

---
+++ src/day1.rs
@@ -0,0 +1,7 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+use std::num::ParseIntError;
+
+#[aoc_generator(day1)]
+fn parse_input_day1(input: &str) -> Result<Vec<i32>, ParseIntError> {
+    input.lines().map(|l| l.parse()).collect()
+}
