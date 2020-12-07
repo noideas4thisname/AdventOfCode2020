661db2095a331bc373dba016ec3705bd2b57ac1c
noideas4thisname <noideas4thisname@hotmail.com>
2020-12-07 14:24:02 -0800

    day3 won't compile example

---
+++ src/day3.rs
@@ -0,0 +1,15 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+
+#[aoc_generator(day3)]
+fn parse_input_day3(input: &str) -> Vec<&str> {
+    let input_lines:Vec<&str> = input.split('\n').collect();
+    return input_lines;
+}
+
+#[aoc(day3, part1)]
+fn part1(input: Vec<&str>) -> u32 {
+
+    println!("{}", input[0]);
+
+    return 0;
+}
