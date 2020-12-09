e4ef55d58d31fd10e627a379e36a68f2e159e8d6
noideas4thisname <noideas4thisname@hotmail.com>
2020-12-08 16:53:26 -0800

    day 6 bones

---
+++ src/day6.rs
@@ -0,0 +1,11 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+
+#[aoc_generator(day6)]
+fn parse_input_day6(input: &str) -> u32 {
+    return 0;
+}
+
+#[aoc(day6, part1)]
+fn part1(input: &str) -> u32 {
+    return 0;
+}
